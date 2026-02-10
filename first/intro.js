ab39b5c46b1584f3b1755149654f8b023fd2a892
maiaadarshhoon <adarshmishra010205@gmail.com>
2026-02-10 05:00:39 +0000

    changes done

--- first/intro.js
+++ first/intro.js
@@ -1,1 +1,19 @@
-console.log("aadarsh");
+//console.log("aadarsh");
+
+// let a=5;
+// let b=10;
+
+// console.log(a);
+// console.log(b);
+
+
+// const a=5;
+// let b=10;
+
+// console.log(a);
+// console.log(b);
+
+let n=5;
+    for(i=1;i<=10;i++){
+        console.log(n + "*" + i + "=" + n*i);
+    }
